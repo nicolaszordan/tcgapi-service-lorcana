f8751e7ad4987813771944be8d59a02b4ac1853b
Nicolas Zordan <nicolas.zordan.tek@gmail.com>
2025-10-16 18:33:06 +0200

    feat: enhance logging for server startup and health check

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"log/slog"
 	"net/http"
 )
 
 func main() {
+	slog.Info("tcgapi-service-lorcana starting")
+
 	http.HandleFunc("/health", health)
-	http.ListenAndServe(":8000", nil)
+
+	slog.Info("starting server on :8000")
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		slog.Error("server error", "error", err)
+	}
 }
 
 func health(w http.ResponseWriter, r *http.Request) {
+	slog.Info("health check")
+
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
 }
